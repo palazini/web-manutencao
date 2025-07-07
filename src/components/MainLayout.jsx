8134ba45057fcd0ede8efea04737a04e3fc74eb2
Gabriel Soares Palazini (SXS BR) <gabriel.palazini@br.spiraxsarco.com>
2025-07-07 13:46:59 -0300

    Man gestor3

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -13,7 +13,7 @@ import MaintainerDashboard from './MaintainerDashboard.jsx';
 import GestorDashboard from './GestorDashboard.jsx';
 import ChamadoDetalhe from '../pages/ChamadoDetalhe.jsx';
 import HistoricoPage from '../pages/HistoricoPage.jsx';
-import PerfilPage from '../pages/PerfilPage.jsx'; // A importação que faltava
+import PerfilPage from '../pages/PerfilPage.jsx'; // A importação necessária
 
 const MainLayout = ({ user }) => {
   const handleLogout = () => {
